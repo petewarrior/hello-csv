bd671ef5173943d2f2995a36e006ba09481f53f5
Arya Antaputra <arya@petewarrior.com>
2016-06-05 02:17:08 +0700

    first try for parse-stream

--- discussion.md
+++ discussion.md
@@ -1,3 +1,9 @@
-The naive() function
+Discussion
+==========
 
-No checking whether CSV is found nor valid.
+The naive() function
+--------------------
+
+* No checking whether CSV is found nor valid.
+* The whole file must be loaded before processing, which is inefficient and even impossible if the file is larger than available memory.
+* Line processing is handled synchronously, hampering performance.
--- parse-stream.js
+++ parse-stream.js
@@ -1,3 +1,72 @@
 // 0. Please use readline (https://nodejs.org/api/readline.html) to deal with per line file reading
 // 1. Then use the parse API of csv-parse (http://csv.adaltas.com/parse/ find the Node.js Stream API section)
 
+'use strict';
+
+const debug = require('debug')('parse-stream');
+
+const readline = require('readline');
+const fs = require('fs');
+const parse = require('csv-parse');
+const helper = require('./helper');
+
+
+//Create the parser
+const parser = parse({
+	  //columns: true
+});
+
+//Use the writable stream api
+parser.on('data', function(record){
+	if(record[0] == 'first_name') return false;
+	debug('Line from file: ', record);
+	let full_name = record[0] + ' '  + record[1];
+	//debug('Full name: ', full_name);
+    let newLine = [ full_name ].concat(record.slice(2));
+	debug('Combined line: ', newLine);
+	helper.sendSms(newLine, function afterSending(err, sendingStatus) {
+        let lineToLog;
+        if (err) {
+            debug(err.message);
+
+            lineToLog = {
+                sendingStatus,
+                newLine,
+            };
+        }
+
+        if (lineToLog) {
+            helper.logToS3(lineToLog, function afterLogging(err, loggingStatus) {
+                if (err) {
+                    debug(err.message);
+                }
+            });
+        }
+    });
+});
+
+
+// Catch any error
+parser.on('error', function(err){
+  debug(err.message);
+});
+
+const input = fs.createReadStream('./sample.csv');
+
+const rl = readline.createInterface({
+  input: input
+});
+
+
+
+rl.on('line', function parseLine(line){
+	//debug(line);
+	parser.write(line);
+	parser.write('\n');
+});
+
+rl.on('close', function closeReadline() {
+	parser.end();
+});
+
+//stream.pipe(rl);
