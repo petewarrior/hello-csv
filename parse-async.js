bdd4b106ed72a3ae6714bc31ab0690670dfd852f
Arya Antaputra <arya@petewarrior.com>
2016-06-29 21:54:38 +0700

    parse stream still full of commented code

--- parse-async.js
+++ parse-async.js
@@ -1,1 +1,20 @@
 // Please use async lib https://github.com/caolan/async
+'use strict';
+
+const debug = require('debug')('parse-async');
+
+const readline = require('readline');
+const fs = require('fs');
+const async = require('async');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('sample.csv')
+});
+
+
+rl.on('line', function parseLine(line){
+});
+
+rl.on('close', function closeReadline() {
+
+});
--- parse-stream.js
+++ parse-stream.js
@@ -9,64 +9,84 @@ const readline = require('readline');
 const fs = require('fs');
 const parse = require('csv-parse');
 const helper = require('./helper');
+const transform = require('stream-transform');
 
+const input = fs.createReadStream('./sample.csv');
 
 //Create the parser
-const parser = parse({
-	  //columns: true
-});
+const parser = parse();
 
 //Use the writable stream api
-parser.on('data', function(record){
-	if(record[0] == 'first_name') return false;
-	debug('Line from file: ', record);
-	let full_name = record[0] + ' '  + record[1];
-	//debug('Full name: ', full_name);
-    let newLine = [ full_name ].concat(record.slice(2));
-	debug('Combined line: ', newLine);
-	helper.sendSms(newLine, function afterSending(err, sendingStatus) {
-        let lineToLog;
-        if (err) {
-            debug(err.message);
+//parser.on('data', function () {
+var createFullNameRecord = transform(function(record) {
+    if (record[0] == 'first_name') return false;
+    //debug('Line from file: ', record);
+    let fullName = record[0] + ' '  + record[1];
 
+    let newLine = [fullName].concat(record.slice(2));
+    //debug('Combined line: ', newLine);
+    return record;
+}, {parallel: 100});
+
+var printErr = function(err) {
+    debug(err.message);
+}
+
+var sendSms = transform(helper.sendSms, {parallel: 100});
+sendSms.on('error', printErr)
+
+var sendSms_ = transform(function(newLine) {
+    var lineToLog;
+    helper.sendSms(newLine, function afterSending(err, sendingStatus) {
+        if (err) {
+            //debug('to log: ', err.message);
+        
             lineToLog = {
                 sendingStatus,
                 newLine,
             };
         }
+    });
 
-        if (lineToLog) {
+    if(lineToLog) debug('lineToLog: ', lineToLog);
+    return lineToLog;
+
+}, {parallel: 20});
+
+var logToS3 = transform(helper.logToS3, {parallel: 100});
+logToS3.on('error', printErr);
+
+var logToS3 = transform(function(lineToLog) {
+    debug('lineToLog: ', lineToLog);
+      if (lineToLog) {
+            
             helper.logToS3(lineToLog, function afterLogging(err, loggingStatus) {
                 if (err) {
                     debug(err.message);
                 }
+
+                debug(loggingStatus);
             });
         }
-    });
-});
-
+}, {parallel: 20});
 
 // Catch any error
-parser.on('error', function(err){
-  debug(err.message);
-});
-
-const input = fs.createReadStream('./sample.csv');
+parser.on('error', printErr);
 
 const rl = readline.createInterface({
-  input: input
+    input: input,
+    output: parser
 });
 
-
-
-rl.on('line', function parseLine(line){
-	//debug(line);
-	parser.write(line);
-	parser.write('\n');
-});
+// rl.on('line', function parseLine(line) {
+// //    debug('line: ', line);
+//      //parser.write(line);
+// //     parser.write('\n');
+// });
 
 rl.on('close', function closeReadline() {
-	parser.end();
+    parser.end();
 });
 
 //stream.pipe(rl);
+input.pipe(parser).pipe(createFullNameRecord).pipe(sendSms).pipe(logToS3);
